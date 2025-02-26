06858267fcabbf65d22211628a8a8295a6701c4d
Belle Sangthong <belle.sangthong@iths.se>
2025-02-26 13:39:12 +0100

    Initial commit

---
+++ src/main/java/belle/sangthong/citytimetracker/CityTimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package belle.sangthong.citytimetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityTimeTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityTimeTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/belle/sangthong/citytimetracker/CityTimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package belle.sangthong.citytimetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityTimeTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
