fcd54b13c649ecceab6d0142ec22d24c3227f6ec
Belle Sangthong <belle.sangthong@iths.se>
2025-02-26 13:52:52 +0100

    Created a time controller

---
+++ src/main/java/belle/sangthong/citytimetracker/TimeController.java
@@ -0,0 +1,20 @@
+package belle.sangthong.citytimetracker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TimeController {
+
+    @Autowired
+    TimeService tService;
+
+    @GetMapping("/citytimetracker")
+    String time(Model model) {
+
+        model.addAttribute("thetime", tService.getCurrentTime());
+        return "timepage";
+    }
+}
