ad99d3fb82a99b409c633776475fa222c250c66d
Belle Sangthong <belle.sangthong@iths.se>
2025-02-28 09:39:57 +0100

    Deleted something

--- src/main/java/belle/sangthong/citytimetracker/TimeService.java
+++ src/main/java/belle/sangthong/citytimetracker/TimeService.java
@@ -6,12 +6,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @Service
 public class TimeService {
-
-
     Map<String,Integer> cities = Map.of("tokyo",8, "bangkok",6, "new york",-6);
 
     ArrayList<String> cityList = new ArrayList<>();
